a7a54bae8dfc1263ff3cf92d70a5f0fca454493c
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-11-17 17:33:09 +0100

    add formular

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
+const add = document.getElementById('cssbuttons-io-button');
+const formuler = document.getElementById('formuler');
+const home = document.getElementById('home');
 
+add.addEventListener('click', function() {
+    home.style.display = 'none';
+    formuler.style.display = 'block';
+});
