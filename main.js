4a4021b5fe7d3f0495097acd4253665f73b6e492
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-11-20 11:40:59 +0100

    modify info-popup add edit and remove btn and show the pic in the info-popup

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ submit.addEventListener('click', function(event) {
     const num = document.getElementById('num').value.trim();
     const pic = document.getElementById('pic').value.trim();
     const domaine = document.getElementById('Domaine').value.trim(); 
+    const experience = document.getElementById('Experience').value.trim();
+    const location = document.getElementById('Location').value.trim();
+   
 
     const newp = document.createElement('div');
     newp.classList.add('card');  // ✔ Add card class!!
@@ -25,11 +28,41 @@ submit.addEventListener('click', function(event) {
   newp.innerHTML = `
     <img id="card_load" src="${pic ? pic : 'https://randomuser.me/api/portraits/men/75.jpg'}" alt="Profile Picture">
     <div id="card_load_extreme_title">${name}</div>
-    <div id="card_load_extreme_descripion">${domaine}</div>
+    <div id="btn">
+    <button id="edit">EDIT</button>
+    <button id="remove">REMOVE</button>
+    </div>
 `;
 
 
+
     cards.appendChild(newp);
+    
+let removeBtn = newp.querySelector('#remove');
+let editBtn = newp.querySelector('#edit');
+
+// REMOVE
+removeBtn.addEventListener('click', function(event) {
+    event.stopPropagation(); // prevents clicking the card
+    cards.removeChild(newp);
+});
+
+// EDIT
+editBtn.addEventListener('click', function(event) {
+    event.stopPropagation(); // prevents opening the popup
+
+    formuler.style.display = 'flex';
+
+    document.getElementById('name').value = name;
+    document.getElementById('email').value = email;
+    document.getElementById('num').value = num;
+    document.getElementById('pic').value = pic;
+    document.getElementById('Domaine').value = domaine;
+    document.getElementById('Experience').value = experience;
+    document.getElementById('Location').value = location;
+
+    cards.removeChild(newp);
+});
     formuler.style.display = 'none';
 
 newp.addEventListener('click', function () {
@@ -45,11 +78,15 @@ newp.addEventListener('click', function () {
     info.classList.add("info-popup");
 
     info.innerHTML = `
-        <h2>Employee Info</h2>
+         <h2>Employee Info</h2>
+        <img src="${pic ? pic : 'https://randomuser.me/api/portraits/men/75.jpg'}" alt="Profile Picture" style="width:100px; height:100px; border-radius:50%; object-fit:cover;">
+   
         <p><strong>Name:</strong> ${name}</p>
         <p><strong>Email:</strong> ${email}</p>
         <p><strong>Phone Number:</strong> ${num}</p>
         <p><strong>Domaine:</strong> ${domaine}</p>
+        <p><strong>Experience:</strong> ${experience}</p>
+        <p><strong>Location:</strong> ${location}</p>
         <button id="closeInfo">Close</button>
     `;
 
@@ -64,4 +101,5 @@ newp.addEventListener('click', function () {
 
 
 });
+
 
